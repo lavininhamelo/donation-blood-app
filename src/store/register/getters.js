8411978d75c738f36019fa3e70deecf5a386d79e
ianito <iaan.developer@gmail.com>
2019-09-23 18:16:52 -0300

    getters of reason reject registration

---
+++ src/store/register/getters.js
@@ -0,0 +1,10 @@
+export const getReasonReject = state => {
+  let allReasons = [];
+  if (state.kilograms < 50) {
+    allReasons.push("kilograms");
+  }
+  if (state.hasInfection) {
+    allReasons.push("hasInfection");
+  }
+  return allReasons;
+};
