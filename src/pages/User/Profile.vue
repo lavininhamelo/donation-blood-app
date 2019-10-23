d11fb21e84f1da62eb2dfdd05d63f177b63b3fac
ianito <iaan.developer@gmail.com>
2019-10-23 12:12:31 -0300

    Move to Up Panel

--- src/pages/User/Profile.vue
+++ src/pages/User/Profile.vue
@@ -1,105 +1,77 @@
 <template>
   <div class="container" v-bind:style="{height: this.$q.screen.height+'px'}">
-
-<div   v-touch-pan.vertical.prevent.mouse="handlePan"
-      class="text-white teste " :style="{ top: currentPosition+'px'}"
+    <div id="information_header" style="opacity:1">
+      <div class="header" v-bind:style="{height: this.$q.screen.height*0.6+'px'}">
+        <div class="img">
+          <img :src="imageProfile" class="profile-img" />
+        </div>
+        <div class="name">{{first_name}} {{last_name}}</div>
+        <div class="user-data">
+          <div class="info">
+            <div class="bottom">
+              <div class="text-red q-gutter-md" style="font-size: 1.2em">
+                <q-icon name="fas fa-tint" />
+              </div>
+            </div>
+            <div class="subdata">
+              <div class="points">3</div>
+              <div class="title">Doações</div>
+            </div>
+          </div>
+          <div class="info middle">
+            <div class="bottom">
+              <div class="text-red q-gutter-md" style="font-size: 1.2em">
+                <q-icon name="far fa-heart" />
+              </div>
+            </div>
+            <div class="subdata">
+              <div class="points">5</div>
+              <div class="title">Vidas salvas</div>
+            </div>
+          </div>
+          <div class="info">
+            <div class="bottom">
+              <div class="text-red q-gutter-md" style="font-size: 1.2em">
+                <q-icon name="fas fa-award" />
+              </div>
+            </div>
+            <div class="subdata">
+              <div class="points">7</div>
+              <div class="title">Conquistas</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="badges" v-bind:style="{height: this.$q.screen.height*0.4+'px'}">
+        <div class="title">Conquistas</div>
+        <div class="bcards">
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+          <div class="bcard"></div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="content teste"
+      :style="{ top: currentPosition+'px'}"
+      v-touch-swipe.mouse.up.down="handleSwipe"
+      ref="contentPanel"
     >
-
-
-    </div>
-
-   <div id="information_header" style="opacity:1">
-    <div class="header" v-bind:style="{height: this.$q.screen.height*0.6+'px'}">
-      <div class="img">
-        <img
-          :src="imageProfile"
-          class="profile-img"
-        >
-      </div>
-      <div class="name">
-        {{first_name}} {{last_name}}
-      </div>
-      <div class="user-data">
-        <div class="info">
-          <div class="bottom">
-            <div
-              class="text-red q-gutter-md"
-              style="font-size: 1.2em"
-            >
-              <q-icon name="fas fa-tint" />
-
-            </div>
-          </div>
-          <div class="subdata">
-            <div class="points">3</div>
-            <div class="title">
-              Doações
-            </div>
-          </div>
-        </div>
-        <div class="info middle">
-          <div class="bottom">
-            <div
-              class="text-red q-gutter-md"
-              style="font-size: 1.2em"
-            >
-              <q-icon name="far fa-heart" />
-
-            </div>
-          </div>
-          <div class="subdata">
-            <div class="points">5</div>
-            <div class="title">
-              Vidas salvas
-            </div>
-          </div>
-        </div>
-        <div class="info">
-          <div class="bottom">
-            <div
-              class="text-red q-gutter-md"
-              style="font-size: 1.2em"
-            >
-              <q-icon name="fas fa-award" />
-
-            </div>
-          </div>
-          <div class="subdata">
-            <div class="points">7</div>
-            <div class="title">
-              Conquistas
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <div class="badges" v-bind:style="{height: this.$q.screen.height*0.4+'px'}">
-      <div class="title">Conquistas</div>
-      <div class="bcards">
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-        <div class="bcard"></div>
-
-      </div>
-
-    </div>
-     </div>
-  
- 
-    <div class="content teste" :style="{ top: currentPosition+'px'}" v-touch-pan.vertical.prevent.mouse="handlePan">
       <div class="menu">
-      <div class="tab-name">doe sangue</div>
-      <div class="tab-name">minhas doações</div>
-      <div class="tab-name">locais</div>
+        <div class="tab-name">doe sangue</div>
+        <div class="tab-name">minhas doações</div>
+        <div class="tab-name">locais</div>
+      </div>
+      <div class="card text-center" v-bind:style="{height: this.$q.screen.height+'px'}">
+        <span>Ola</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil labore harum quibusdam quidem error! Modi deleniti vel eum quo magnam, animi, voluptates sed voluptatibus sint in ratione aspernatur velit esse.</p>
+      </div>
     </div>
-    <div class="card" v-bind:style="{height: this.$q.screen.height*2.8+'px'}"></div>
-    </div>
-
   </div>
 </template>
 
@@ -109,11 +81,12 @@ import Router from "vue-router";
 
 export default {
   name: "Profile",
-  data () {
+  data() {
     return {
       info: null,
       panning: false,
-      currentPosition: this.$q.screen.height*0.80,
+
+      currentPosition: this.$q.screen.height * 0.85,
       email: auth.currentUser.email,
       first_name: null,
       last_name: null,
@@ -125,71 +98,78 @@ export default {
     };
   },
   methods: {
-     handlePan ({ evt, ...info }) {
-       let marginBottom = 100
-       let {y} = info.offset
-     this.currentPosition = info.position.top
-       if(this.currentPosition  <= 0 ){
-        this.currentPosition = 0
-       } else if(this.currentPosition >= this.$q.screen.height - marginBottom){
-         this.currentPosition = this.$q.screen.height - marginBottom
-       }
+    movePanel(toUp) {
+      let marginBottom = 0.85;
+      let elem = this.$refs.contentPanel;
 
-       },
-    logout () {
+      if (toUp) {
+        let pos = this.$q.screen.height * marginBottom;
+        let id = setInterval(frame, 1);
+
+        function frame() {
+          if (pos <= 0) {
+            clearInterval(id);
+          } else {
+            pos -= 5.5;
+            elem.style.top = pos + "px";
+          }
+        }
+      } else {
+      }
+    },
+    handleSwipe({ evt, ...info }) {
+      if (info.direction === "up") {
+        this.movePanel(true);
+      } else {
+        this.movePanel(false);
+      }
+    },
+    logout() {
       const r = this;
       auth
         .signOut()
         .then(() => r.$router.push("/"))
-        .catch(function (error) {
+        .catch(function(error) {
           const e = `${error.code} - ${error.message}`;
           console.log(e);
         });
     },
-    userData () {
+    userData() {
       const user = auth.currentUser;
       if (user) {
-
         db.collection("Users")
           .doc(user.uid)
           .get()
           .then(doc => {
-            this.email = user.email
+            this.email = user.email;
             this.first_name = doc.data().first_name;
             this.last_name = doc.data().last_name;
             this.displayName = user.displayName;
             this.blood_type = doc.data().blood_type;
             this.years = doc.data().years;
             this.bio = doc.data().bio;
-            console.log(user)
+            console.log(user);
           })
-          .catch(function (error) {
+          .catch(function(error) {
             const e = `${error.code} - ${error.message}`;
             console.log(e);
           });
       } else {
         this.$router.push("/");
       }
-
     }
-
   },
-  mounted () {
+  mounted() {
     this.userData();
-
   }
-}
-
-
+};
 </script>
 <style lang="stylus" scoped>
-
-.teste{
-
+.teste {
   width: 600px;
   height: 600px;
   position: absolute;
-  background-color:#9b3;
+  background-color: blue;
 }
 
 .container {
@@ -206,7 +186,6 @@ export default {
   justify-content: center;
   align-items: center;
   background: #EA5A44;
-
   -webkit-clip-path: ellipse(110% 50% at 50% 40%);
   clip-path: ellipse(110% 50% at 50% 40%);
 }
@@ -299,7 +278,6 @@ export default {
   flex-direction: column;
   align-items: center;
   background: #E3391F;
-
 }
 
 .badges .title {
@@ -317,7 +295,6 @@ export default {
   width: 85%;
   justify-content: flex-start;
   overflow: auto;
-
 }
 
 .badges .bcards .bcard {
@@ -329,34 +306,33 @@ export default {
   border-radius: 12px;
 }
 
-.content{
+.content {
   position: absolute;
   width: 100%;
   top: 80%;
+  z-index: 10;
 }
 
-.content .menu{
-  margin: 20px 0px 14px 0px ;
-width: 100%;
-
+.content .menu {
+  margin: 20px 0px 20px 0px;
+  width: 100%;
   display: flex;
   font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 10px;
-line-height: 12px;
-justify-content: space-around;
-text-transform: uppercase;
-width: 0 auto;
-color: #FFFFFF;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 12px;
+  justify-content: space-around;
+  text-transform: uppercase;
+  width: 0 auto;
+  color: #FFFFFF;
 }
 
-.content .card{
+.content .card {
   background: #fff;
   width: 100%;
   border-radius: 5% 5% 0% 0%;
   box-shadow: 0px -1px 5px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
-
 }
 </style>
