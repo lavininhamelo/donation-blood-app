6d9cd41e10b2241ce7a54258a325acc83634d1ba
lavininhamelo <lavininhamelo@hotmail.com>
2020-04-23 20:16:47 -0400

    Redirecionamento para o login

--- src/pages/Auth/Steps/StepPassword.vue
+++ src/pages/Auth/Steps/StepPassword.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <div class="text-center q-mt-lg">
-      <small class="answer block full-width" style="color: #F59A8C;">Criando sua conta...</small>
+      <small class="answer block full-width" style="color: #F59A8C;"
+        >Criando sua conta...</small
+      >
     </div>
     <h5 class="text-center">Por favor, insira sua senha:</h5>
     <form @submit.prevent="goToNextStep">
       <div class="content absolute-center text-center">
-        <q-input v-model="password" type="password" color="white" maxlength="24" dark />
+        <q-input
+          v-model="password"
+          type="password"
+          color="white"
+          maxlength="24"
+          dark
+        />
       </div>
       <div class="footer absolute-bottom q-px-lg q-mb-xl">
         <q-btn
@@ -19,7 +27,11 @@
           label="PROXIMA"
           style="height:45px;"
         >
-          <q-icon class="absolute-right q-ma-sm" name="keyboard_arrow_right" size="30px" />
+          <q-icon
+            class="absolute-right q-ma-sm"
+            name="keyboard_arrow_right"
+            size="30px"
+          />
         </q-btn>
       </div>
     </form>
@@ -29,7 +41,9 @@
           <div class="text-h6">Error!</div>
         </q-card-section>
 
-        <q-card-section>Por favor, preencha o campo corretamente.</q-card-section>
+        <q-card-section
+          >Por favor, preencha o campo corretamente.</q-card-section
+        >
 
         <q-card-actions align="right">
           <q-btn flat label="OK" color="primary" v-close-popup />
@@ -98,6 +112,7 @@ export default {
           .then(() => {
             console.log("Usuario Criado com sucesso.");
             this.sending = false;
+            this.$router.push("/");
           })
           .catch(e => {
             console.log(e);
